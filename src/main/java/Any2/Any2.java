35dfe5f9046de4692bca47c06cb842d8be2dffcb
Jasmeen <kaurjasmin579@gmail.com>
2020-02-11 11:15:36 -0500

    seventh

---
+++ src/main/java/Any2/Any2.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Any2;
+
+/**
+ *
+ * @author Jasmeen
+ */
+public class Any2 {
+    private int a;
+    private String str;
+
+    public Any2() {
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public String getStr() {
+        return str;
+    }
+    
+    
+}
